0da83eedd7cdb8da0f8d94a9cfe42ef97ee1bb50
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-09 23:01:10 +0900

    [Feat] Schedule periodic fetch in `OpenApiScheduler.java`

--- src/main/java/com/boombim/openapi/OpenApiScheduler.java
+++ src/main/java/com/boombim/openapi/OpenApiScheduler.java
@@ -1,5 +1,23 @@
 package com.boombim.openapi;
 
+import com.boombim.openapi.service.OpenApiService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class OpenApiScheduler {
+
+    private final OpenApiService openApiService;
+
+    @Scheduled(cron = "30 */5 * * * *")
+    public void fetch() {
+        log.info("[OpenApiScheduler] fetch 시작");
+        openApiService.fetchAndSave();
+    }
 
 }
