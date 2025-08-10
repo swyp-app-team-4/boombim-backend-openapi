2611b526ec372a7f018a70d3944b09d8c27a6fb0
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-10 16:22:07 +0900

    [Chore] Add `data.sql`

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,10 @@
+-- congestion_levels 테이블 초기 데이터
+
+INSERT INTO congestion_levels (id, name, message)
+VALUES (1, '여유', '사람이 몰려있을 가능성이 낮고 붐빔은 거의 느껴지지 않아요. 도보 이동이 자유로워요.');
+INSERT INTO congestion_levels (id, name, message)
+VALUES (2, '보통', '사람이 몰려있을 수 있지만 크게 붐비지는 않아요. 도보 이동에 큰 제약이 없어요.');
+INSERT INTO congestion_levels (id, name, message)
+VALUES (3, '약간 붐빔', '사람들이 몰려있을 가능성이 크고 붐빈다고 느낄 수 있어요. 인구 밀도가 높은 구간에서는 도보 이동시 부딪힘이 발생할 수 있어요.');
+INSERT INTO congestion_levels (id, name, message)
+VALUES (4, '붐빔', '사람들이 몰려있을 가능성이 매우 크고 많이 붐빈다고 느낄 수 있어요. 인구 밀도가 높은 구간에서는 도보 이동시 부딪힘이 발생할 수 있어요.');
