bedb001872e9507d8fee441ad0f0d7d816dead19
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-10 21:53:47 +0900

    [Refactor] Switch forecast insert to use `official_place_id` instead of `poi_code` in `OfficialCongestionForecastDao.java`

--- src/main/java/com/boombim/congestion/repository/OfficialCongestionForecastDao.java
+++ src/main/java/com/boombim/congestion/repository/OfficialCongestionForecastDao.java
@@ -16,14 +16,16 @@ public class OfficialCongestionForecastDao {
      *
      * @param batchArgs 서비스 계층에서 모든 파라미터가 준비된 Object 배열 리스트
      */
-    public void saveAll(List<Object[]> batchArgs) {
+    public void saveAll(
+        List<Object[]> batchArgs
+    ) {
         if (batchArgs == null || batchArgs.isEmpty()) {
             return;
         }
 
         String sql = """
             INSERT INTO official_congestion_forecasts (
-                poi_code, observed_at, forecast_time, forecast_congestion_level_id,
+                official_place_id, observed_at, forecast_time, forecast_congestion_level_id,
                 forecast_population_min, forecast_population_max
             )
             VALUES (?, ?, ?, ?, ?, ?)
