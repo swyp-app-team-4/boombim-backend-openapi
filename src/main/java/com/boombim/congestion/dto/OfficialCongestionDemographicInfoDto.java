946c59fdd4c905bb1c6386716b0fb723784093b7
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-08-10 16:55:49 +0900

    [Feat] Define DTO for demographic info in `OfficialCongestionDemographicInfoDto.java`

--- src/main/java/com/boombim/congestion/dto/OfficialCongestionDemographicInfoDto.java
+++ src/main/java/com/boombim/congestion/dto/OfficialCongestionDemographicInfoDto.java
@@ -1,5 +1,17 @@
 package com.boombim.congestion.dto;
 
-public record OfficialCongestionDemographicInfoDto() {
+/**
+ *
+ * 인구 통계 정보를 전달하기 위한 DTO
+ *
+ * @param category    통계 대분류 (ex. "GENDER", "AGE_GROUP")
+ * @param subCategory 통계 소분류 (ex. "MALE", "FEMALE")
+ * @param rate        해당 비율 값
+ */
+public record OfficialCongestionDemographicInfoDto(
+    String category,
+    String subCategory,
+    Double rate
+) {
 
 }
