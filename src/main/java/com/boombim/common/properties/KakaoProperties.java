8b8f185dc5027be75cd42fbcdb355fac63c0ab30
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-05 14:24:51 +0900

    [Feat] Add KakaoProperties configuration record with fields in `KakaoProperties.java`

--- src/main/java/com/boombim/common/properties/KakaoProperties.java
+++ src/main/java/com/boombim/common/properties/KakaoProperties.java
@@ -1,5 +1,12 @@
 package com.boombim.common.properties;
 
-public record KakaoProperties() {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "kakao")
+public record KakaoProperties(
+    String baseUrl,
+    String scheme,
+    String apiKey
+) {
 
 }
