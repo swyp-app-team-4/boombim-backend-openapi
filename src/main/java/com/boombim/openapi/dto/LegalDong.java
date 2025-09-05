76df0a59c9ea26b1a3e3d8b1eb57c759c7a2080f
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-05 15:02:20 +0900

    [Feat] Add LegalDong record with display method for formatted output in `LegalDong.java`

--- src/main/java/com/boombim/openapi/dto/LegalDong.java
+++ src/main/java/com/boombim/openapi/dto/LegalDong.java
@@ -1,5 +1,13 @@
 package com.boombim.openapi.dto;
 
-public record LegalDong() {
+public record LegalDong(
+    String gu,
+    String dong,
+    String code
+) {
+
+    public String display() {
+        return gu + " " + dong;
+    }
 
 }
