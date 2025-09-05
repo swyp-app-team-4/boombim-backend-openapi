574babd87c4c9bfe663d259feeb570e72b019573
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-05 14:25:23 +0900

    [Feat] Add Kakao WebClient bean with timeout and Authorization header in `WebClientConfig.java`

--- src/main/java/com/boombim/common/config/WebClientConfig.java
+++ src/main/java/com/boombim/common/config/WebClientConfig.java
@@ -1,10 +1,15 @@
 package com.boombim.common.config;
 
 import static com.boombim.common.constant.OpenApiConstant.*;
+import static org.springframework.http.HttpHeaders.*;
 
+import com.boombim.common.properties.KakaoProperties;
+import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 
 @Configuration
 public class WebClientConfig {
@@ -17,4 +22,17 @@ public class WebClientConfig {
             .build();
     }
 
+    @Bean
+    public WebClient kakaoWebClient(
+        KakaoProperties properties
+    ) {
+        HttpClient httpClient = HttpClient.create()
+            .responseTimeout(Duration.ofSeconds(3));
+
+        return WebClient.builder()
+            .baseUrl(properties.baseUrl())
+            .defaultHeader(AUTHORIZATION, properties.scheme() + properties.apiKey())
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .build();
+    }
 }
