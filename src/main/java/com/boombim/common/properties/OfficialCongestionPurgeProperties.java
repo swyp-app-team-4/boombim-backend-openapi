51c3115562bbccab4f5a9d89a3efaf58a409bc97
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-09-13 15:14:13 +0900

    [Feat] Add `OfficialCongestionPurgeProperties` with `@ConfigurationProperties` in `OfficialCongestionPurgeProperties.java`

--- src/main/java/com/boombim/common/properties/OfficialCongestionPurgeProperties.java
+++ src/main/java/com/boombim/common/properties/OfficialCongestionPurgeProperties.java
@@ -1,5 +1,12 @@
 package com.boombim.common.properties;
 
-public record OfficialCongestionPurgeProperties() {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "official-congestion.purge")
+public record OfficialCongestionPurgeProperties(
+    boolean enabled,
+    int days,
+    int batchSize
+) {
 
 }
