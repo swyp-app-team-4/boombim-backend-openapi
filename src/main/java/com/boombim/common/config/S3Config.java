488f8f336947fdb6189a143bf51cecdac7f1c409
GOMUNGNAM <rhrjsgh97@gmail.com>
2025-12-07 22:01:45 +0900

    [Refactor] Remove static credentials in `S3Config.java`

--- src/main/java/com/boombim/common/config/S3Config.java
+++ src/main/java/com/boombim/common/config/S3Config.java
@@ -6,10 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 
 @Slf4j
 @Configuration
@@ -22,16 +21,10 @@ public class S3Config {
     @Bean
     public S3Client s3Client() {
 
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(
-            s3Properties.accessKey(),
-            s3Properties.secretKey()
-        );
-
         return S3Client.builder()
             .region(Region.of(s3Properties.region()))
-            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .credentialsProvider(DefaultCredentialsProvider.create())
             .build();
-
     }
 
 }
